05b6f27c2e35c40e886efd7df3f6f3af8cffad08
otrofama <otrofama@gmail.com>
2016-06-05 15:41:05 -0500

    rm sad.c,lk.c

--- linkedlist.c
+++
@@ -1,6 +0,0 @@
-int main(int argc, char const *argv[]) {
-    printf("%ss\n", );
-    return 0;
-}
-weqeqdsasadasdas
-//http://www.xnxx.com/video16732683/me_folle_temprano_a_la_prima_para_quitar_la_calentura
