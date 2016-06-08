89e039ab0f3528c2ae97ec0b234a4796d56b1fa5
otrofama <otrofama@gmail.com>
2016-06-07 21:20:16 -0500

    4th comm

--- typetable.h
+++ typetable.h
@@ -1,27 +1,23 @@
-/**
- *
- *
- *
- *
- */
-
 #ifndef TYPE_TABLE_H
 #define TYPE_TABLE_H
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+#define TAM_TTABLE 100
+////////////////////////////////////
+
+#if 0
 typedef struct TypeTable Types;
 typedef struct Tupla Tupla;
 typedef struct Nodo Nodo;
 
 
-#if 0
 struct Tupla
 {
 	char* str;
 	int token;
 };
-#endif
 struct Tupla
 {
 	int pos;
@@ -52,5 +48,55 @@ void print(Types* types);
 /**
  * void error(char* msg);
  */
+
+#endif
+
+////////////////////////////////////
+struct _type
+{
+	int type;
+	int dim;
+	int widht;
+	int base;
+};
+struct _type_table
+{
+	type _type_table[TAM_TTABLE];
+	int cont;
+};
+typedef struct _type type;
+Ttable Tstack[TAM_TTABLE];
+int insert_type(Ttable* table,type t)
+{
+	table.t[cont]=t;
+	table.cont++;
+	return table.cont-1;
+}
+
+type create_type(int type,int dim,int widht,int base)
+{
+	type tmp;
+	tmp.type=type;
+	tmp.dim=dim;
+	tmp.widht=widht;
+	tmp.base=base;
+	return tmp;
+}
+void create_type_table(Ttable* table)
+{
+	type t_char = create_type(0,-1,1,-1);
+	type t_int = create_type(1,-1,4,-1);
+	type t_float = create_type(1,-1,8,-1);
+	type t_double = create_type(2,-1,16,-1);
+	type t_array = create_type(3,-1,1,-1);
+	insert_type(tabla,t_int);
+	insert_type(tabla,t_float);
+	insert_type(tabla,t_double);
+	insert_type(tabla,t_array);
+	insert_type(tabla,t_char);
+	/*type t_float = create_type(2,-1,8,-1);
+	type t_doble = create_type(3,-1,16,-1);*/
+}
+
 
 #endif
